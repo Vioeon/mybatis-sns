0a0eb6f9ece2c123c95bd92b0f9bdd6cbe48e180
Lee SeungEon <dltmddjs98@naver.com>
2026-09-15 14:16:25 +0900

    update

--- src/test/java/net/likelion/bebc25/sns/service/PostServiceTest.java
+++ src/test/java/net/likelion/bebc25/sns/service/PostServiceTest.java
@@ -67,7 +67,7 @@ class PostServiceTest {
         PostUpdateRequest updateDto = new PostUpdateRequest("수정된 비즈니스 본문 내용", "https://image.com/updated.jpg");
 
         // when
-        postService.updatePostd(postId, updateDto);
+        postService.updatePost(postId, updateDto);
         PostResponse updatedPost = postService.getPostById(postId);
 
         // then
